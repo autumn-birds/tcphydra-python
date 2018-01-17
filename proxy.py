be4b6e48815900e06ea0a95c5bd7eff98b33df5d
curiousbirds <curiousbirds@users.noreply.github.com>
2018-01-17 00:32:24 -0800

    Refactor. Add more things. Still very WIP.

--- proxy.py
+++ proxy.py
@@ -1,9 +1,6 @@
 # vim:tabstop=3 shiftwidth=3 expandtab autoindent
 
-# 'Telnet' multiplexer for MUDs, etc.
-
-# Convention: Normal Python strings EVERYWHERE, only convert to bytes when about to call
-# send/receive on an actual socket.
+# 'Telnet' multiplexer for MUDs, etc. Python 3.
 
 # Things we should be doing someday:
 #  - actually understand the underlying protocols (Telnet, etc.) to keep clients from
@@ -23,10 +20,40 @@ servers = {}             # index of available servers by display name
 connections = {}         # index of currently connected server & client objects by socket
 
 
-class LineBufferingSocketContainer:
-   def __init__(self):
-      self.__send_buffer = ''
+class TextLine:
+   def __init__(self, string, encoding):
+      assert type(string) == bytes or type(string) == str
+      self.__enc = encoding
 
+      if type(string) == bytes:
+         self.__raw = string
+      else:
+         self.__raw = string.encode(encoding)
+
+   def as_str(self):
+      """Try to 'safely', but lossily, decode the raw line into an ordinary string,
+      according to the encoding given."""
+      s = ""
+      r = self.__raw
+
+      while len(r) > 0:
+         try:
+            s += r.decode(self.__enc)
+            r = ''
+         except UnicodeDecodeError as e:
+            s += r[:e.start].decode(self.__enc)
+
+            for byte in self.__enc[e.start:e.end]:
+               s += '?(' + __repr__(byte) + ')'
+
+            r = r[e.end:]
+
+   def as_bytes(self):
+      return self.__raw
+
+
+class LineBufferingSocketContainer:
+   def __init__(self, socket = None):
       self.__b_send_buffer = b''
       self.__b_recv_buffer = b''
 
@@ -37,45 +64,47 @@ class LineBufferingSocketContainer:
       self.encoding = ENCODING
       self.linesep = LINE_SEPARATOR
 
-   def write(self, data=None):
-      # Should quietly buffer any overflow, then (try to) write as many complete 
-      # lines as it can
-      # Fail on !self.connected
-      # If data=None, then just try to write anything left in the buffer into the
-      # socket
+      if socket != None:
+         self.attach_socket(socket)
+
+   def write_str(self):
+      assert type(data) == str
+
+      self.__b_send_buffer += data.encode(self.encoding)
+
+      self.flush()
+
+   def write_line(self, line):
+      assert type(line) == TextLine
+
+      self.__b_send_buffer += line.as_bytes()
+
+      self.flush()
+
+   def write(self, data):
+      assert type(data) == bytes
+
+      self.__b_send_buffer += data
+
+      self.flush()
+
+   def flush(self):
+      assert self.socket != None
       assert self.connected
 
-      if data != None:
-         assert type(data) == str
-         self.__send_buffer += data
-
-      while len(self.__send_buffer) > 0:
-         x = 0
-         while x < len(self.__send_buffer):
-            if self.__send_buffer[x] == '\n':
-               self.__b_send_buffer += self.__send_buffer[:x+1].encode(self.encoding)
-               self.__send_buffer = self.__send_buffer[x+1:]
-               break
-            x += 1
-         if x >= len(self.__send_buffer):
-            break # no more newlines
-
-      print(repr(self.__b_send_buffer))
-
-      if len(self.__b_send_buffer) > 0:
-         assert self.socket != None
+      while len(self.__b_send_buffer) > 0 and self.linesep in self.__b_send_buffer:
          try:
-            n_bytes = self.socket.write(self.__b_send_buffer)
+            t = self.__b_send_buffer.index(self.linesep)
+            n_bytes = self.socket.write(self.__b_send_buffer[:t+1])
             self.__b_send_buffer = self.__b_send_buffer[n_bytes:]
          except BlockingIOError:
-            pass
+            break
 
    def read(self):
-      # Should read all it can, but return None if there's not a complete
-      # line yet (or [] -- return list of lines?)
-      # Fail on !self.connected
       assert self.connected
       assert self.socket != None
+
+      has_eof = False
 
       try:
          data = b''
@@ -85,6 +114,8 @@ class LineBufferingSocketContainer:
             if len(data) < RECV_MAX:
                # May need to watch out for len(data)==0 which might mean the remote side
                # closed the connection?
+               if len(data) == 0:
+                  has_eof = True
                break
             data = b''
       except BlockingIOError:
@@ -92,29 +123,16 @@ class LineBufferingSocketContainer:
 
       q = []
 
-      # Currently, we just assume a particular byte separates lines.
-
-      # This isn't the very best plan available, but it's better than blindly trying to decode
-      # whatever chunk we have at the moment and then separate it, because of multi-byte
-      # characters in some encodings: The network is allowed to only send us however many
-      # bytes it wants, and there might be a partial character in there.
-
       # The best we can do for a record separator in this case is a byte or byte sequence that
       # means 'newline'. We go with one byte for now for simplicity & because it works with
       # UTF-8/ASCII at least, which comprises most things we're interested in.
 
-      while len(self.__b_recv_buffer) > 0:
-         x = 0
-         while x < len(self.__b_recv_buffer):
-            if self.__b_recv_buffer[x] == self.linesep:
-               q += [self.__b_recv_buffer[:x+1].decode(self.encoding)]
-               self.__b_recv_buffer = self.__b_recv_buffer[x+1:]
-               break
-            x += 1
-         if x >= len(self.__b_recv_buffer):
-            break # no more newlines
+      while self.linesep in self.__b_recv_buffer:
+         t = self.__b_recv_buffer.index(self.linesep)
+         q += TextLine(self.__b_recv_buffer[:t+1], self.encoding)
+         self.__b_recv_buffer = self.__b_recv_buffer[t+1:]
 
-      return q
+      return (q, has_eof)
 
    def attach_socket(self, socket):
       socket.setblocking(False)
